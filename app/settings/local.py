1abfeb1ece53b7eb11b7426c4ff8125f6b9bd775
Atul Mishra <atulmishra.one@gmail.com>
2018-09-16 08:55:56 +0530

    Change Directory path

--- app/settings/local.py
+++ app/settings/local.py
@@ -2,8 +2,8 @@ from .base import *
 
 STATIC_URL = 'http://127.0.0.1:8080/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
 MEDIA_URL = 'http://127.0.0.1:8080/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
